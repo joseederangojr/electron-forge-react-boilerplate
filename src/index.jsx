8ea649e8267e57427d8fff91b658614d25590eb3
Jose Ederango Jr <joseederangojr@gmail.com>
2021-04-24 02:36:12 +0800

    chore: setup development environment

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./components/App.jsx";
+import React from "react"
+import { render } from "react-dom"
+import App from "./components/App.jsx"
 
 render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.querySelector("#root")
-);
+)
